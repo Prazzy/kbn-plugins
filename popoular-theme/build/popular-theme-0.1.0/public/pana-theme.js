67cbb94d7ef5b650ff6f551cf2400c3703115894
Praveen <prazzee@gmail.com>
2016-03-08 10:49:37 -0800

    added popular-theme plugin

---
+++ popoular-theme/build/popular-theme-0.1.0/public/pana-theme.js
@@ -0,0 +1,14 @@
+/**
+ * Created by praveen on 2/26/16.
+ */
+
+import chrome from 'ui/chrome';
+
+chrome
+.setNavBackground('red')
+.setTabs([
+  {
+    id: 'dashboard',
+    title: 'Dashboard'
+  }
+]);
---
+++ popoular-theme/public/pana-theme.js
@@ -0,0 +1,9 @@
+/**
+ * Created by praveen on 2/26/16.
+ */
+
+import chrome from 'ui/chrome';
+import kibanaLogoUrl from 'plugins/popular-theme/images/PanasonicLogo.png';
+
+chrome
+.setNavBackground('#08519c');
