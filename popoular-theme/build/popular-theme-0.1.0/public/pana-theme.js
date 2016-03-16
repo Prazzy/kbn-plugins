ee3a97b1c4fb9ec592a06b071d3b48ff321bc138
Praveen <prazzee@gmail.com>
2016-03-16 15:30:20 -0700

    remove

--- popoular-theme/build/popular-theme-0.1.0/public/pana-theme.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by praveen on 2/26/16.
- */
-
-import chrome from 'ui/chrome';
-import kibanaLogoUrl from 'plugins/popular-theme/images/PanasonicLogo.png';
-
-chrome
-.setNavBackground('#08519c');
